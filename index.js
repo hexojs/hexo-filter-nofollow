1090aa3603f83d003744e667bb00895ba77e7c66
SukkaW <isukkaw@gmail.com>
2019-09-14 17:26:02 +0800

    refactor: add default config using Object.assign

--- index.js
+++ index.js
@@ -2,13 +2,21 @@
 
 'use strict';
 
-const config = Object.assign({
-  enable: true
+hexo.config.nofollow = Object.assign({
+  enable: true,
+  field: 'site',
+  exclude: []
 }, hexo.config.nofollow);
 
-if (!config.enable) return;
+let exclude = hexo.config.nofollow.exclude;
+if (exclude && !Array.isArray(exclude)) {
+  exclude = [exclude];
+  hexo.config.nofollow.exclude = exclude;
+}
 
-if (!config.field) config.field = 'site';
+const config = hexo.config.nofollow;
+
+if (!config.enable) return;
 
 if (config.field === 'post') {
   hexo.extend.filter.register('after_post_render', require('./lib/filter'));
