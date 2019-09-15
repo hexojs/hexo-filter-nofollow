c0b00169cffc840e13cf33eff7f4ce1b32683a10
Sukka <isukkaw@gmail.com>
2019-09-15 18:59:23 +0800

    refactor: enabled by default (#3)
    * refactor: enabled by default
    * refactor: add default config using Object.assign
    * refactor: exclude conversion without touching hexo.config
    * fix: minor changes
    
    Apply suggestions from code review by @curbengh

--- index.js
+++ index.js
@@ -2,14 +2,18 @@
 
 'use strict';
 
-if (hexo.config.nofollow && hexo.config.nofollow.enable) {
-  const config = hexo.config.nofollow;
+hexo.config.nofollow = Object.assign({
+  enable: true,
+  field: 'site',
+  exclude: []
+}, hexo.config.nofollow);
 
-  if (!config.field) config.field = 'site';
+const config = hexo.config.nofollow;
 
-  if (config.field === 'post') {
-    hexo.extend.filter.register('after_post_render', require('./lib/filter'));
-  } else {
-    hexo.extend.filter.register('after_render:html', require('./lib/filter'));
-  }
+if (!config.enable) return;
+
+if (config.field === 'post') {
+  hexo.extend.filter.register('after_post_render', require('./lib/filter'));
+} else {
+  hexo.extend.filter.register('after_render:html', require('./lib/filter'));
 }
